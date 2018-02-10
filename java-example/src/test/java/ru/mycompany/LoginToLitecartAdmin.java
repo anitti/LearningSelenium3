5fd4e010b8337c5bbbbcec201a60914abf4343b4
Anya <galayko.as@gmail.com>
2018-02-10 23:02:59 +0300

    Login to litecart admin
    Exercise 3

---
+++ java-example/src/test/java/ru/mycompany/LoginToLitecartAdmin.java
@@ -0,0 +1,37 @@
+package ru.mycompany;
+
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginToLitecartAdmin {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void loginToAdmin() {
+        String username = "admin";
+        String password = "admin";
+
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys(username);
+        driver.findElement(By.name("password")).sendKeys(password);
+        driver.findElement(By.name("login")).click();
+
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
